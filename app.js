f3b18c6bfd71079854b5079b825633db879fa107
unknown <data.luitel@gmail.com>
2019-12-22 19:36:58 -0800

    Server is created

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 
-const Logger = require('./logger'); // load the file logger
- const logger = new Logger(); // creating the object of class Logger
+const http = require('http');// load the http module  or all the methods find in module in http
+const server = http.createServer( function (req,res){
+    if(req.url==="/"){
+        res.write('Hello world ');
+        res.end();
+    }
+    if(req.url==='/api/courses'){
+       res.write(JSON.stringify([1,2,3,4,5,6]));
+       res.end();       
+    }
 
- // Register a listener
-logger.on('logging',eventArg =>{
-    console.log('Listener is called', eventArg);
-})
+});
 
-logger.log('message');
+//server.on('connection', (socket)=>{ // register the events raised 
+//console.log('New connection......');
+//})
+
+
+
+server.listen(3000); // listining on the port 3000 or raised the event 
